7b2782dd89df455562603efb1b20d2061622fbc5
Mhirii <ahmedmhiri218@gmail.com>
2023-12-18 03:05:19 +0100

    auth

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,11 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/auth',
+      name: 'auth',
+      component: () => import('../views/LoginView.vue')
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
