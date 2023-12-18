01aa713d9ed3a801d01c826fadcc2ef6022716d9
Mhirii <ahmedmhiri218@gmail.com>
2023-12-18 03:04:36 +0100

    signup and login

---
+++ src/components/FromField.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { ref } from "vue"
+defineProps(['type', 'placeholder', 'name', 'required'])
+const value = ref('')
+</script>
+
+<template>
+  <div class="form-field">
+    <label :for="name"> {{ name }} </label>
+    <input v-model="value" :type="type" :placeholder="placeholder" :name="name" :required="required"
+      @input="$emit('update-value', value)" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.form-field {
+  @apply flex flex-col items-start;
+
+  label {
+    @apply text-base capitalize text-slate-200/50
+  }
+
+  input {
+    @apply bg-slate-800 px-2 py-1 rounded focus:bg-indigo-700/25 focus:outline-none focus:shadow hover:bg-slate-800/75 border border-slate-700/70 focus:border-indigo-500
+  }
+}
+</style>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import axios from 'axios'
+
+import FormField from "./FromField.vue"
+
+const email = ref("");
+const password = ref("");
+const headers = {
+  'Content-Type': 'application/json',
+  "Access-Control-Allow-Origin": "*",
+};
+const login = async () => {
+  const response = await axios.post("https://hono.ahmedmhiri218.workers.dev/login", {
+    email: email.value,
+    password: password.value
+  }, { headers })
+  console.log(response.data)
+}
+</script>
+
+<template>
+  <form action="" :onsubmit="(e /*@ts-ignore e is implicit any */) => e.preventDefault()"
+    class="flex flex-col gap-4 items-center">
+    <h2 class="text-2xl text-indigo-200">Welcome Back</h2>
+
+    <FormField name="email" placeholder="Email" type="email" :required="true" @update-value="(v) => (email = v)" />
+    <FormField name="password" placeholder="Password" type="password" :required="true"
+      @update-value="(v) => (password = v)" />
+
+    <button type="submit"
+      class="p-2 w-full bg-indigo-700 text-base font-medium rounded hover:shadow-sm hover:bg-indigo-700/50 hover:text-indigo-300 border border-indigo-500/50 hover:border-indigo-500"
+      @click="async () => { await login() }">
+      Login
+    </button>
+  </form>
+</template>
+
+<style lang="scss" scoped></style>
