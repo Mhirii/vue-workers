3f4eca1042286461ce219401030d9d8d504e68ca
Mhirii <ahmedmhiri218@gmail.com>
2023-12-19 18:52:56 +0100

    dashboard route

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,6 +15,11 @@ const router = createRouter({
       component: () => import('../views/LoginView.vue')
     },
     {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('../views/DashboardView.vue')
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
