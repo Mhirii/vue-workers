707dce979960dbcd3df794454c17e2159fe97cad
Mhirii <ahmedmhiri218@gmail.com>
2023-12-19 18:52:32 +0100

    dashboard init

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { useAuth } from '@/stores/auth';
+
+const auth = useAuth()
+</script>
+
+<template>
+  <div>
+    this is the dashboard
+    <div>{{ auth.username }}</div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
